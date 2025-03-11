2309548e7b49658b56c7862fad990ce292e43576
David Pearce <davepearce5819@gmail.com>
2025-03-11 13:02:07 -0500

    Model fitting is done. Need to add in extracting abundance/density information for plots.

--- Scripts/3.1_Heli_Transect_distancePkg.R
+++ Scripts/3.1_Heli_Transect_distancePkg.R
@@ -1,9 +1,15 @@
-# This script extends the script and vignette written by Eric Rexstad
-# for line transect distance sampling. Which can be found here: 
-# https://distancesampling.org/Distance/articles/lines-distill.html
-# This script analyses 4 helicopter surveys with two replicates per
-# survey to estimate white-tailed deer abundance.
-# 
+# Author: David L. Pearce
+# Description:
+#             This script extends the script and vignette written by Eric Rexstad
+#             for line transect distance sampling. Which can be found here: 
+#             https://distancesampling.org/Distance/articles/lines-distill.html
+#             This script analyses 4 helicopter surveys with two replicates per
+#             survey to estimate white-tailed deer abundance.             
+
+# Citation: 
+#           TBD
+
+
 
 # ------------------------------------------------------------------------------
 #
@@ -177,7 +183,7 @@ print(model_AIC)
 # Can see hazard rate is the better model
 
 # Goodness of fit
-gof_ds(F23_hr_null, main = "Hazard-rate Null") # Chi-square shows a pretty good fit
+gof_ds(F23_hr_null) 
 
 # ----------------------
 # Covariates
@@ -239,7 +245,7 @@ print(model_AIC)
 plot(F23_hr_fit3, breaks= c(0,20,40,60,100), main="Fall '23")
 
 # Goodness of fit
-gof_ds(F23_hr_null, main = "Hazard-rate Null") # Chi-square shows a pretty good fit
+gof_ds(F23_hr_null) 
 
 
 # Getting estimates of abundance
@@ -290,10 +296,10 @@ model_AIC <- model_AIC[order(model_AIC$AIC), ] # Order
 print(model_AIC)
 
 
-# Can see hazard rate is the better model
+# Can see Half-normal is the better model
 
 # Goodness of fit
-gof_ds(W24_hn_null, main = "Hazard-rate Null") # Chi-square shows a pretty good fit
+gof_ds(W24_hn_null) 
 
 # ----------------------
 # Covariates
@@ -352,10 +358,10 @@ print(model_AIC)
 # Best model was Fit1: Group size
 
 # Plot detection probability by distance bin
-plot(W24_hn_fit1, breaks= c(0,20,40,60,100), main = "WInter '24")
+plot(W24_hn_fit1, breaks= c(0,20,40,60,100), main = "Winter '24")
 
 # Goodness of fit
-gof_ds(W24_hn_fit1, main = "Hazard-rate Null") # Chi-square shows a pretty good fit
+gof_ds(W24_hn_fit1) 
 
 
 # Getting estimates of abundance
@@ -407,10 +413,10 @@ model_AIC <- model_AIC[order(model_AIC$AIC), ] # Order
 print(model_AIC)
 
 
-# Can see hazard rate is the better model
+# Can see Half-Normal is the better model
 
 # Goodness of fit
-gof_ds(F24_hn_null, main = "Hazard-rate Null") # Chi-square shows a pretty good fit
+gof_ds(F24_hn_null)  
 
 # ----------------------
 # Covariates
@@ -466,17 +472,17 @@ model_AIC <- AIC(F24_hn_null, F24_hn_fit1, F24_hn_fit2, F24_hn_fit3, F24_hn_fit4
 model_AIC <- model_AIC[order(model_AIC$AIC), ] # Order 
 print(model_AIC)
 
-# Best model was Fit1: Group size
+# Best model was Fit2: Replicate/Survey Time
 
 # Plot detection probability by distance bin
-plot(F24_hn_fit1, breaks= c(0,20,40,60,100), main = "WInter '24")
+plot(F24_hn_fit2, breaks= c(0, 20, 40, 60, 100), main = "Fall '24")
 
 # Goodness of fit
-gof_ds(F24_hn_fit1, main = "Hazard-rate Null") # Chi-square shows a pretty good fit
+gof_ds(F24_hn_fit2) # Chi-square shows a pretty good fit
 
 
 # Getting estimates of abundance
-F24_abund <-  dht2(ddf = F24_hn_fit1$ddf,
+F24_abund <-  dht2(ddf = F24_hn_fit2$ddf,
                    flatfile = fall24_dat,
                    stratification = "replicate",
                    strat_formula = ~ replicate,
@@ -484,5 +490,125 @@ F24_abund <-  dht2(ddf = F24_hn_fit1$ddf,
 )
 
 
-print(F24_abund, report = "density")   # Density of ~ 0.19 deer per hectare                  
-print(F24_abund, report = "abundance") # Total abundance of 219 on property                       
+print(F24_abund, report = "density")   # Density of ~ 0.15 deer per hectare                  
+print(F24_abund, report = "abundance") # Total abundance of 168 on property                       
+
+
+# -------------------------------------------------------
+#                     Winter 2025
+# -------------------------------------------------------
+
+# ----------------------
+# Detection Function
+# ----------------------
+
+# Half normal detection function model
+W25_hn_null <- ds(data = win25_dat,
+                  formula = ~ 1,      
+                  transect = "line",  
+                  key = "hn",       
+                  dht_group = FALSE,   
+                  adjustment = NULL,  
+                  convert_units = conversion.factor,
+)
+
+# Half normal detection function model
+W25_hr_null <- ds(data = win25_dat,
+                  formula = ~ 1,
+                  transect = "line",  
+                  key = "hr",  
+                  dht_group = FALSE,  
+                  adjustment = NULL, 
+                  convert_units = conversion.factor,
+)
+
+
+# Model comparison table
+model_AIC <- AIC(W25_hn_null, W25_hr_null) # Extract AIC
+model_AIC <- model_AIC[order(model_AIC$AIC), ] # Order 
+print(model_AIC)
+
+
+# Can see Half-Normal is the better model
+
+# Goodness of fit
+gof_ds(W25_hn_null)  
+
+# ----------------------
+# Covariates
+# ----------------------
+
+# Group size
+W25_hn_fit1 <- ds(data = win25_dat, 
+                  formula = ~ size, 
+                  transect = "line",  
+                  key = "hn", 
+                  dht_group = FALSE, 
+                  adjustment = NULL, 
+                  convert_units = conversion.factor,
+)
+
+
+# Replicate (Evening or Morning) 
+W25_hn_fit2 <- ds(data = win25_dat, 
+                  formula = ~ factor(replicate) ,
+                  transect = "line", 
+                  key = "hn",  
+                  dht_group = FALSE,  
+                  adjustment = NULL, 
+                  convert_units = conversion.factor,  
+)
+
+
+# Woody Aggregation Index
+W25_hn_fit3 <- ds(data = win25_dat, 
+                  formula = ~ scale(woody_AggInx),
+                  transect = "line", 
+                  key = "hn",  
+                  dht_group = FALSE, 
+                  adjustment = NULL, 
+                  convert_units = conversion.factor, 
+)
+
+# Woody Largest Patch Index
+W25_hn_fit4 <- ds(data = win25_dat, 
+                  formula = ~ scale(woody_lrgPInx),
+                  transect = "line", 
+                  key = "hn",  
+                  dht_group = FALSE, 
+                  adjustment = NULL, 
+                  convert_units = conversion.factor, 
+)
+
+
+
+
+# Model comparison table
+model_AIC <- AIC(W25_hn_null, W25_hn_fit1, W25_hn_fit2, W25_hn_fit3, W25_hn_fit4) # Extract AIC
+model_AIC <- model_AIC[order(model_AIC$AIC), ] # Order 
+print(model_AIC)
+
+# Best model was Null: No covariates on detection
+
+# Plot detection probability by distance bin
+plot(W25_hn_null, breaks= c(0, 20, 40, 60, 100), main = "Winter '25")
+
+# Goodness of fit
+gof_ds(W25_hn_null) # Chi-square shows a pretty good fit
+
+
+# Getting estimates of abundance
+W25_abund <-  dht2(ddf = W25_hn_null$ddf,
+                   flatfile = win25_dat,
+                   stratification = "replicate",
+                   strat_formula = ~ replicate,
+                   convert_units = conversion.factor
+)
+
+
+print(W25_abund, report = "density")   # Density of ~ 0.21 deer per hectare                  
+print(W25_abund, report = "abundance") # Total abundance of 239 on property                       
+
+
+
+# ----------------------------- End of Script -----------------------------
