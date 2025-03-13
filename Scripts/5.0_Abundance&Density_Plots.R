d0f83b31b85a1de84ef37937f3f6c266d964da08
David Pearce <davepearce5819@gmail.com>
2025-03-13 12:23:23 -0500

    Initial Commit: Script for Abundance and Density Plots.

---
+++ Scripts/5.0_Abundance&Density_Plots.R
@@ -0,0 +1,31 @@
+# Author: David L. Pearce
+# Description:
+#             TBD
+
+# Citation: 
+#      TBD
+
+
+# ------------------------------------------------------------------------------
+#
+#                               Loading R packages
+#
+# ------------------------------------------------------------------------------
+
+## Install packages (if needed)
+# install.packages("tidyverse")
+
+# Load library
+library(tidyverse)
+
+# Set seed, scientific notation options, and working directory
+set.seed(123)
+options(scipen = 9999)
+setwd(".")
+
+
+# ------------------------------------------------------------------------------
+#
+#                                 Reading in data
+#
+# ------------------------------------------------------------------------------
