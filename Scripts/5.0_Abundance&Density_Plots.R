06203ea6dea5fcc4441fb78a30755984d6f6a3fe
David Pearce <davepearce5819@gmail.com>
2025-03-18 16:34:11 -0500

    Added in TTE

--- Scripts/5.0_Abundance&Density_Plots.R
+++ Scripts/5.0_Abundance&Density_Plots.R
@@ -29,7 +29,7 @@ setwd(".")
 #
 # ------------------------------------------------------------------------------
 
-# Fall 2023 Estimates 
+# DS Estimates 
 F23_Heli_DS_est <- readRDS("./Model_Objects/F23_Heli_DS_AbundEst.rds") # Helicopter DS
 F23_Heli_625segDS_est <- readRDS("./Model_Objects/F23_625segHeli_DS_AbundEst.rds")
 F23_Heli_1250segDS_est <- readRDS("./Model_Objects/F23_1250segHeli_DS_AbundEst.rds")
@@ -39,23 +39,23 @@ F23_Heli_1250segHDS_est <- readRDS("./Model_Objects/F23_1250segHeli_HDS_AbundEst
 
 #F23_Cam_Nmix_est <- readRDS("./Model_Objects/F23_Cam_Nmix_AbundEst.rds") # Camera
 
-# Winter 2024 Estimates
+# HDS Estimates
 W24_Heli_DS_est <- readRDS("./Model_Objects/W24_Heli_DS_AbundEst.rds") # Helicopter DS
 W24_Heli_1250segDS_est <- readRDS("./Model_Objects/W24_1250segHeli_DS_AbundEst.rds") 
 W24_Heli_625segDS_est <- readRDS("./Model_Objects/W24_625segHeli_DS_AbundEst.rds")
 W24_Heli_HDS_est <- readRDS("./Model_Objects/W24_Heli_HDS_AbundEst.rds") # Helicopter HDS
 W24_Heli_625segHDS_est <- readRDS("./Model_Objects/W24_625segHeli_HDS_AbundEst.rds")
 W24_Heli_1250segHDS_est <- readRDS("./Model_Objects/W24_1250segHeli_HDS_AbundEst.rds")
-#W24_Cam_Nmix_est <- readRDS("./Model_Objects/W24_Cam_Nmix_AbundEst.rds") # Camera
+#W24_Cam_TTE_est <- readRDS("./Model_Objects/W24_Cam_Nmix_AbundEst.rds") # Camera
 
-# Fall 2024 Estimates 
+# Cam Estimates 
 F24_Heli_DS_est <- readRDS("./Model_Objects/F24_Heli_DS_AbundEst.rds") # Helicopter DS
 F24_Heli_1250segDS_est <- readRDS("./Model_Objects/F24_1250segHeli_DS_AbundEst.rds")
 F24_Heli_625segDS_est <- readRDS("./Model_Objects/F24_625segHeli_DS_AbundEst.rds")
 F24_Heli_HDS_est <- readRDS("./Model_Objects/F24_Heli_HDS_AbundEst.rds")
 F24_Heli_625segHDS_est <- readRDS("./Model_Objects/F24_625segHeli_HDS_AbundEst.rds")
 F24_Heli_1250segHDS_est <- readRDS("./Model_Objects/F24_1250segHeli_HDS_AbundEst.rds")
-#F24_Cam_Nmix_est <- readRDS("./Model_Objects/F24_Cam_Nmix_AbundEst.rds") # Camera
+#F24_Cam_TTE_est <- readRDS("./Model_Objects/F24_Cam_Nmix_AbundEst.rds") # Camera
 
 
 # Winter 2025 Estimates
@@ -66,6 +66,9 @@ W25_Heli_HDS_est <- readRDS("./Model_Objects/W25_Heli_HDS_AbundEst.rds") # Helic
 W25_Heli_625segHDS_est <- readRDS("./Model_Objects/W25_625segHeli_HDS_AbundEst.rds")
 W25_Heli_1250segHDS_est <- readRDS("./Model_Objects/W25_1250segHeli_HDS_AbundEst.rds")
 # W25_Cam_Nmix <- readRDS() # Camera
+
+# Cam Estimates
+Cam_TTE_est <- readRDS("./Model_Objects/Cam_TTE_AbundEst.rds")
 
 # Renaming model this will need deleted  *********************************
 F23_Heli_625segHDS_est$Model <- "Heli 625seg HDS"
@@ -149,70 +152,38 @@ All_625segHDS_est <- rbind(F23_Heli_625segHDS_est, W24_Heli_625segHDS_est, F24_H
 print(All_625segHDS_est)
 
 
-# # -------------------------------
-# # Organizing Nmix Estimates 
-# # -------------------------------
-# 
-# # Combining all Camera Nmix estimates
-# All_Cam_Nmix <- rbind(F23_Cam_Nmix_est, W24_Cam_Nmix_est, F24_Cam_Nmix_est)
-# 
-# # Take a look
-# print(All_Cam_Nmix)
-
-# # -------------------------------
-# # Combine Across Surveys and models 
-# # -------------------------------
-# 
-# # Combine all estimates
-# All_estimates <- rbind(All_DS_est, All_Heli_HDS, All_1250segDS_est, All_625segDS_est, All_Cam_Nmix) 
-# 
-# # Season and models are factors for plotting 
-# All_estimates <- All_estimates %>%
-#   mutate(Season_Model = factor(Season_Model, 
-#                                levels = c(
-#                                  
-#                         # Fall 2023
-#                         "F23 Heli DS", "F23 Heli HDS", "F23 1250seg Heli DS", "F23 625seg Heli DS", "F23 Cam Nmix",
-#                         
-#                         # Winter 2024
-#                         "W24 Heli DS", "W24 Heli HDS", "W24 1250seg Heli DS", "W24 625seg Heli DS", "W24 Cam Nmix",
-#                         
-#                         # Fall 2024
-#                         "F24 Heli DS", "F24 Heli HDS", "F24 1250seg Heli DS", "F24 625seg Heli DS", "F24 Cam Nmix",
-#                         
-#                         # Winter 2025
-#                         "W25 Heli DS", "W25 Heli HDS", "W25 1250seg Heli DS", "W25 625seg Heli DS"
-#                         )) 
-# )  
-# 
-# # Take a look
-# print(All_estimates)
-
 # -------------------------------
-# All heli 
+# Combine Across Surveys and models
 # -------------------------------
 
 # Combine all helicopter models
-all_heli <- rbind(All_DS_est, All_1250segDS_est, All_625segDS_est, All_Heli_HDS, All_1250segHDS_est, All_625segHDS_est)
+all_est <- rbind(All_DS_est, 
+                  All_1250segDS_est, 
+                  All_625segDS_est, 
+                  All_Heli_HDS, 
+                  All_1250segHDS_est, 
+                  All_625segHDS_est,
+                  Cam_TTE_est)
 
-# Factor by models                 
-all_heli <- all_heli %>%
-  mutate(Model = factor(Model, 
-                               levels = c(
-                                 "Heli DS", 
-                                 "1250seg Heli DS", 
-                                 "625seg Heli DS", 
-                                 "Heli HDS", 
-                                 "Heli 1250seg HDS",
-                                 "Heli 625seg HDS" 
-                               )) 
+# Factor by models
+all_est <- all_est %>%
+  mutate(Model = factor(Model,
+                        levels = c(
+                         "Heli DS",
+                         "1250seg Heli DS",
+                         "625seg Heli DS",
+                         "Heli HDS",
+                         "Heli 1250seg HDS",
+                         "Heli 625seg HDS",
+                         "Cam TTE"
+                       ))
   )
 
 # Subset by season
-F23_All_Heli <- all_heli[which(all_heli$Season == "Fall 2023"),]
-W24_All_Heli <- all_heli[which(all_heli$Season == "Winter 2024"),]
-F24_All_Heli <- all_heli[which(all_heli$Season == "Fall 2024"),]
-W25_All_Heli <- all_heli[which(all_heli$Season == "Winter 2025"),]
+F23_est <- all_est[which(all_est$Season == "Fall 2023"),]
+W24_est <- all_est[which(all_est$Season == "Winter 2024"),]
+F24_est <- all_est[which(all_est$Season == "Fall 2024"),]
+W25_est <- all_est[which(all_est$Season == "Winter 2025"),]
 
 # -------------------------------
 # Abundance Estimate Plot By Season
@@ -228,7 +199,9 @@ col_palette <- c(
                  
                  "Heli HDS" = "#1F3A93", # Blue
                  "Heli 1250seg HDS" = "#E41A1C", # Red
-                 "Heli 625seg HDS" = "#CC79A7" # Reddish Purple
+                 "Heli 625seg HDS" = "#CC79A7", # Reddish Purple
+                 
+                 "Cam TTE" = "#009E73"  # Bluish Green
 )   
 
 # Define Custom shape 
@@ -243,12 +216,14 @@ shape_palette <- c(
   
   "Heli HDS" = 18,  # Diamond for HDS
   "Heli 1250seg HDS" = 18, 
-  "Heli 625seg HDS" = 18  
+  "Heli 625seg HDS" = 18, 
+  
+  "Cam TTE" = 15 # Square for TTE
 ) 
 
 
 # Fall 2023
-F23_heli_abund_plot <- ggplot(F23_All_Heli, aes(x = Model, y = N, color = Model, shape = Model)) +
+F23_abund_plot <- ggplot(F23_est, aes(x = Model, y = N, color = Model, shape = Model)) +
                 geom_point(size = 4) +  
                 geom_errorbar(aes(ymin = LCI, ymax = UCI), width = 0.2) +   
                 theme_minimal() +
@@ -257,7 +232,7 @@ F23_heli_abund_plot <- ggplot(F23_All_Heli, aes(x = Model, y = N, color = Model,
                      y = "Abundance Estimate") +
                 scale_color_manual(values = col_palette) +   
                 scale_shape_manual(values = shape_palette) +    
-                scale_y_continuous(limits = c(100, 1700), breaks = seq(100, 1700, by = 100)) +    
+                scale_y_continuous(limits = c(0, 1700), breaks = seq(0, 1700, by = 100)) +    
                 theme(
                   legend.position = "none", 
                   axis.text.x = element_text(size = 12, angle = 45, hjust = 1),   
@@ -270,17 +245,17 @@ F23_heli_abund_plot <- ggplot(F23_All_Heli, aes(x = Model, y = N, color = Model,
 )
 
 # View
-print(F23_heli_abund_plot)
+print(F23_abund_plot)
 
 
 # Export
-ggsave(plot = F23_heli_abund_plot, 
-       "./Figures/Abundance_Plots/F23_heli_abund_plot.jpeg", 
+ggsave(plot = F23_abund_plot, 
+       "./Figures/Abundance_Plots/F23_abund_plot.jpeg", 
        width = 10, height = 5, dpi = 300) 
 
 
 # Winter 2024
-W24_heli_abund_plot <- ggplot(W24_All_Heli, aes(x = Model, y = N, color = Model, shape = Model)) +
+W24_abund_plot <- ggplot(W24_est, aes(x = Model, y = N, color = Model, shape = Model)) +
                         geom_point(size = 4) +  
                         geom_errorbar(aes(ymin = LCI, ymax = UCI), width = 0.2) +   
                         theme_minimal() +
@@ -289,7 +264,7 @@ W24_heli_abund_plot <- ggplot(W24_All_Heli, aes(x = Model, y = N, color = Model,
                              y = "Abundance Estimate") +
                         scale_color_manual(values = col_palette) +   
                         scale_shape_manual(values = shape_palette) +   
-                        scale_y_continuous(limits = c(100, 1100), breaks = seq(100, 1100, by = 100)) +   
+                        scale_y_continuous(limits = c(0, 1100), breaks = seq(0, 1100, by = 100)) +   
                         theme(
                           legend.position = "none", 
                           axis.text.x = element_text(size = 12, angle = 45, hjust = 1),   
@@ -302,16 +277,17 @@ W24_heli_abund_plot <- ggplot(W24_All_Heli, aes(x = Model, y = N, color = Model,
 )
 
 # View
-print(W24_heli_abund_plot)
+print(W24_abund_plot)
 
 
 # Export
-ggsave(plot = W24_heli_abund_plot, 
-       "./Figures/Abundance_Plots/W24_heli_abund_plot.jpeg", 
+ggsave(plot = W24_abund_plot, 
+       "./Figures/Abundance_Plots/W24_abund_plot.jpeg", 
        width = 10, height = 5, dpi = 300) 
 
+
 # Fall 2024
-F24_heli_abund_plot <- ggplot(F24_All_Heli, aes(x = Model, y = N, color = Model, shape = Model)) +
+F24_abund_plot <- ggplot(F24_est, aes(x = Model, y = N, color = Model, shape = Model)) +
                         geom_point(size = 4) +  
                         geom_errorbar(aes(ymin = LCI, ymax = UCI), width = 0.2) +   
                         theme_minimal() +
@@ -320,7 +296,7 @@ F24_heli_abund_plot <- ggplot(F24_All_Heli, aes(x = Model, y = N, color = Model,
                              y = "Abundance Estimate") +
                         scale_color_manual(values = col_palette) +   
                         scale_shape_manual(values = shape_palette) +  
-                        scale_y_continuous(limits = c(100, 800), breaks = seq(100, 800, by = 100)) +   
+                        scale_y_continuous(limits = c(0, 800), breaks = seq(0, 800, by = 100)) +   
                         theme(
                           legend.position = "none", 
                           axis.text.x = element_text(size = 12, angle = 45, hjust = 1),   
@@ -333,16 +309,18 @@ F24_heli_abund_plot <- ggplot(F24_All_Heli, aes(x = Model, y = N, color = Model,
 )
 
 # View
-print(F24_heli_abund_plot)
+print(F24_abund_plot)
 
 
 # Export
-ggsave(plot = F24_heli_abund_plot, 
-       "./Figures/Abundance_Plots/F24_heli_abund_plot.jpeg", 
+ggsave(plot = F24_abund_plot, 
+       "./Figures/Abundance_Plots/F24_abund_plot.jpeg", 
        width = 10, height = 5, dpi = 300) 
 
+
+
 # Winter 2025
-W25_heli_abund_plot <- ggplot(W25_All_Heli, aes(x = Model, y = N, color = Model, shape = Model)) +
+W25_abund_plot <- ggplot(W25_est, aes(x = Model, y = N, color = Model, shape = Model)) +
                         geom_point(size = 4) +  
                         geom_errorbar(aes(ymin = LCI, ymax = UCI), width = 0.2) +   
                         theme_minimal() +
@@ -364,63 +342,15 @@ W25_heli_abund_plot <- ggplot(W25_All_Heli, aes(x = Model, y = N, color = Model,
 )
 
 # View
-print(W25_heli_abund_plot)
+print(W25_abund_plot)
 
 
 # Export
-ggsave(plot = W25_heli_abund_plot, 
-       "./Figures/Abundance_Plots/W25_heli_abund_plot.jpeg", 
+ggsave(plot = W25_abund_plot, 
+       "./Figures/Abundance_Plots/W25_abund_plot.jpeg", 
        width = 10, height = 5, dpi = 300) 
 
  
  
 
-
-
-
-
-
-
-
-# -------------------------------
-# Abundance Estimate Plot  
-# -------------------------------
- 
-# Define custom colors for each model
-col_palette <- c("Heli CF" = "black", 
-                 "Heli DS" = "orange",     
-                 "Heli HDS" = "purple",
-                 "1250seg Heli DS" = "blue",
-                 "625seg Heli DS" = "red",
-                 "Cam Nmix" = "forestgreen",
-                 "Cam TTE" = "yellow"
-)   
-
-
-# Abundance Estimates Fall 2023 - Winter 2025
-abund_plot <- ggplot(all_heli, aes(x = Season_Model, y = N, color = Model)) +
-                      geom_point(size = 4, shape = 16) +  
-                      geom_errorbar(aes(ymin = LCI, ymax = UCI), width = 0.2) +   
-                      theme_minimal() +
-                      labs(title = "",
-                           x = "Model",
-                           y = "Estimate (N)") +
-                      scale_color_manual(values = col_palette) +   
-                      scale_y_continuous(limits = c(100, 900), breaks = seq(100, 900, by = 100)) +   
-                      theme(
-                        legend.position = "none", 
-                        axis.text.x = element_text(size = 12, angle = 45, hjust = 1),   
-                        axis.text.y = element_text(size = 12),   
-                        axis.title.x = element_text(size = 14, margin = margin(t = 10)), 
-                        axis.title.y = element_text(size = 14, margin = margin(r = 10)),  
-                        plot.title = element_text(size = 16, face = "bold", hjust = 0.5),  
-                        panel.grid = element_blank(),  
-                        axis.ticks = element_line(size = 0.8)  
-)
-
-# View
-print(abund_plot)
-
-
-# Export
-ggsave(plot = abund_plot, "./Figures/Abundance_Plots/Comparison_Abundance_Plot.jpeg", width = 10, height = 5, dpi = 300) 
+# ----------------------------- End of Script -----------------------------
